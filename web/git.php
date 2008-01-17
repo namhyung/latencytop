a7d2daeb32ba1b67b59549eb8fd66dca82dea47f
Arjan van de Ven <arjan@linux.intel.com>
2008-01-16 22:42:31 -0700

    more updates to the website

---
+++ web/git.php
@@ -0,0 +1,26 @@
+<? 
+include("topbar.php");
+?>
+
+<table>
+<tr><td valign=top width=200>
+<table>
+<tr><td class=subsection>GENERAL</td></tr>
+<tr><td class='nav_box'><a class='nav' href='/index.php?n=Info'>Info</a></td></tr>
+<tr><td class='nav_box'><a class='nav' href='announce.php'>Announcement</a></td></tr> 
+<tr><td class='nav_box'><a class='nav' href='download.php'>Download</a></td></tr> 
+<tr><td class='nav_box_active'><a class='nav' href='git.php'>GIT Repository</a></td></tr> 
+</table>
+</td>
+<td>
+
+<h3>Git tree</h3>
+The development version of LatencyTOP is available from <a
+href="http://git.infradead.org/?p=latencytop.git">git</a>: <pre> git clone
+git://git.infradead.org/latencytop.git </pre>
+
+</td></tr>
+</table>
+<hr>
+Intel is a trademark of Intel Corporation in the U.S. and other countries. | * Other names / brands may be claimed as the property of others 
+</html>
--- web/index.php
+++ web/index.php
@@ -2,6 +2,17 @@
 include("topbar.php");
 ?>
 
+<table>
+<tr><td valign=top width=200>
+<table>
+<tr><td class=subsection>GENERAL</td></tr>
+<tr><td class='nav_box_active'><a class='nav' href='/index.php?n=Info'>Info</a></td></tr>
+<tr><td class='nav_box'><a class='nav' href='announce.php'>Announcement</a></td></tr> 
+<tr><td class='nav_box'><a class='nav' href='download.php'>Download</a></td></tr> 
+<tr><td class='nav_box'><a class='nav' href='git.php'>GIT Repository</a></td></tr> 
+</table>
+</td>
+<td>
 <h1>About LatencyTOP</h1>
 
 Skipping audio, slower servers, everyone knows the symptoms. But to know
@@ -34,20 +45,9 @@ delays.
 <br><br>
 <img src="latencytop.png">
 
-<h3>Download</h3>
-<table>
-<tr class="odd"><th>File</th><th>Link</th></tr>
-<tr><td>userspace tool</td><td><a class="plainlink" href="download/latencytop-0.1.tar.gz">latencytop-0.1.tar.gz</a></td></tr>
-<tr class="odd"><td>kernel patch 1/3</td><td><a class="plainlink" href="download/latencytop-infrastructure.patch">infrastructure</a></td></tr>
-<tr><td>kernel patch 2/3</td><td><a class="plainlink" href="download/latencytop-annotations.patch">annotations part 1</a></td></tr>
-<tr class="odd"><td>kernel patch 3/3</td><td><a class="plainlink" href="download/latencytop-more-annotations.patch">annotations part 2</a></td></tr>
-</table>
-<h3>Git tree</h3>
-The development version of LatencyTOP is available from <a href="http://git.infradead.org/?p=latencytop.git">git</a>:
-<pre>
-git clone git://git.infradead.org/latencytop.git
-</pre>
 
+</td></tr>
+</table>
 <hr>
 Intel is a trademark of Intel Corporation in the U.S. and other countries. | * Other names / brands may be claimed as the property of others 
 </html>
