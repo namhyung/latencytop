9956c4ca8987b699d48a1da3dba337256bdda622
Arjan van de Ven <arjan@linux.intel.com>
2008-01-20 17:43:47 -0700

    update the website with version 0.2 of latencytop

--- web/download.php
+++ web/download.php
@@ -17,13 +17,10 @@ include("topbar.php");
 <h3>Download</h3>
 <table>
 <tr class="odd"><th>File</th><th>Link</th></tr>
-<tr><td>userspace tool</td><td><a class="plainlink" href="download/latencytop-0.1.tar.gz">latencytop-0.1.tar.gz</a></td></tr>
+<tr><td>userspace tool</td><td><a class="plainlink" href="download/latencytop-0.2.tar.gz">latencytop-0.2.tar.gz</a></td></tr>
 
-<tr class="odd"><td>kernel patch 1/3</td><td><a class="plainlink" href="download/latencytop-infrastructure.patch">infrastructure</a></td></tr>
+<tr class="odd"><td>kernel patch</td><td><a class="plainlink" href="download/latencytop.patch">latencytop.patch</a></td></tr>
 
-<tr><td>kernel patch 2/3</td><td><a class="plainlink" href="download/latencytop-annotations.patch">annotations part 1</a></td></tr>
-
-<tr class="odd"><td>kernel patch 3/3</td><td><a class="plainlink" href="download/latencytop-more-annotations.patch">annotations part 2</a></td></tr>
 </table>
 
 </td></tr>
